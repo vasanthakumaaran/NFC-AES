94e8ad15ad3d1f2701c192fd524194911bec7618
vasanthakumaaran <vasanthakumaaran76@gmail.com>
2022-06-20 18:46:45 +0530

    Added files
    Signed-off-by: vasanthakumaaran <vasanthakumaaran76@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
+const Cryptr = require('cryptr');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+  const [formData,setFormData] = useState('')
+  const [password,setPassword] = useState('')
+
+  const Encrypt = async() => {
+    if(formData.length <= 0 || password.length <= 0){
+      alert('Please fill all the fields')
+    }
+    else {
+    //encrypt the formData 
+    const cryptr = new Cryptr(password);
+    const encrypted = await cryptr.encrypt(formData);
+    await localStorage.setItem('encrypted',encrypted);
+    await console.log(encrypted);
+    await setFormData('');
+    await setPassword('');
+  }
+  }
+
+  const Decrypt = async() => {
+  if(password.length <= 0  || localStorage.getItem('encrypted') === null){
+    alert('Please fill all the fields')
+  } else {
+   if(localStorage.getItem('encrypted') !== null){
+    const cryptr = new Cryptr(password);
+    try{
+    const decrypted = await cryptr.decrypt(localStorage.getItem('encrypted'));
+    await console.log(decrypted);
+    await setFormData(decrypted);
+    } catch(err){
+      alert('Wrong Password')
+    }
+   } else {
+    await console.log("No data to decrypt");
+    await setFormData("No data to decrypt");
+   }
+  }
+  }
+
+
+  const ButtonForm = () => (
+    <>
+      <div style={{display:"flex",flexDirection:"row",gap:"10px"}}>
+        <button onClick={() => Decrypt() } style={{background:"#000000",padding:"1rem 1rem",color:"white"}}>
+          Read
+        </button>
+        <button onClick={() => Encrypt()} style={{background:"#000000",padding:"1rem 1rem",color:"white"}}>
+          Write
+        </button>
+      </div>
+    </>
+  )
+  return(
+    <>
+      
+      <div style={{display:"flex",flexDirection:"column",justifyContent:"center",alignItems:"center",top:'50%'}}>
+      <label>Enter Text</label>
+        <input type="textarea" value={formData} onChange={(e) => setFormData(e.target.value)} />
+        <label>Enter Password</label>
+        <input type="textarea" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <ButtonForm/>
+      </div>
+    </>
+  )
 }
+
+
+
 
 export default App;
